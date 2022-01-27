4bd01e7f8d0b1692b97f636ce224cf59f8ded6c1
Marko <markosuhanov@gmail.com>
2022-01-27 18:53:44 +0100

    ma nemam pojma samo mi stavi nesto

---
+++ Restaurant/src/test/java/com/kts/Restaurant/constants/ItemConstants.java
@@ -0,0 +1,49 @@
+package com.kts.Restaurant.constants;
+
+public class ItemConstants {
+
+    public static final Long DB_NEW_NEWITEM_ID = 10L;
+    public static final String DB_NEW_ITEM_NAME = "Item1";
+    public static final String DB_NEW_NEWITEM_DESCRIPTION = "Item1Description";
+    public static final boolean DB_NEW_NEWITEM_ACTIVE = true;
+    public static final int DB_NEW_NEWITEM_PRICE= 100;
+    public static final int DB_NEW_NEWITEM_COST = 50;
+
+    public static final Long DB_NEW_ITEM_CATEGORY_ID = 51L;
+    public static final String DB_NEW_ITEM_CATEGORY_TYPE = "ItemCategoryType1";
+    public static final String DB_NEW_ITEM_CATEGORY_NAME = "ItemCategoryName1";
+    public static final String DB_NEW_ITEM_CATEGORY_ICON = "ItemCategoryIcon1";
+
+
+
+
+    public static final Long DB_ITEM_ID = 15L;
+    public static final String DB_ITEM_NAME = "Item1Existing";
+    public static final String DB_ITEM_DESCRIPTION = "Item1DescriptionExisting";
+    public static final boolean DB_ITEM_ACTIVE = true;
+    public static final int DB_ITEM_PRICE= 1200;
+    public static final int DB_ITEM_COST = 800;
+
+    public static final Long DB_ITEM_CATEGORY_ID = 51L;
+    public static final String DB_ITEM_CATEGORY_TYPE = "ItemCategoryTypeExisting";
+    public static final String DB_ITEM_CATEGORY_NAME = "ItemCategoryNameExisting";
+    public static final String DB_ITEM_CATEGORY_ICON = "ItemCategoryIconExisting";
+
+
+    public static final String DB_DRINK_ITEM_NAME = "Drink";
+    public static final int DB_DRINK_ITEM_PRICE = 123;
+    public static final int DB_DRINK_ITEM_COST = 70;
+
+    public static final String DB_NEW_DRINK_ITEM_NAME = "DrinkNew";
+    public static final int DB_NEW_DRINK_ITEM_PRICE = 200;
+    public static final int DB_NEW_DRINK_ITEM_COST = 100;
+
+
+    public static final String DB_FOOD_ITEM_NAME = "Food";
+    public static final int DB_FOOD_ITEM_PRICE = 150;
+    public static final int DB_FOOD_ITEM_COST = 80;
+
+    public static final String DB_NEW_FOOD_ITEM_NAME = "FoodNew";
+    public static final int DB_NEW_FOOD_ITEM_PRICE = 200;
+    public static final int DB_NEW_FOOD_ITEM_COST = 100;
+}
--- Restaurant/src/test/java/com/kts/Restaurant/service/ItemServiceUnitTests.java
+++ Restaurant/src/test/java/com/kts/Restaurant/service/ItemServiceUnitTests.java
@@ -1,15 +1,25 @@
 package com.kts.Restaurant.service;
 
+import com.kts.Restaurant.constants.ItemConstants;
+import com.kts.Restaurant.dto.ItemDTO;
 import com.kts.Restaurant.exceptions.ItemCategoryNameDoesntExists;
+import com.kts.Restaurant.exceptions.ItemWithNameAlreadyExistsException;
+import com.kts.Restaurant.model.DrinkItem;
+import com.kts.Restaurant.model.Item;
+import com.kts.Restaurant.model.ItemCategory;
 import com.kts.Restaurant.repository.ItemCategoryRepository;
 import com.kts.Restaurant.repository.ItemRepository;
+import org.junit.Before;
 import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import static com.kts.Restaurant.constants.ItemConstants.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,12 +35,35 @@ public class ItemServiceUnitTests {
     @MockBean
     ItemCategoryRepository itemCategoryRepository;
 
+    ItemDTO itemDto = new ItemDTO(DB_ITEM_NAME, DB_ITEM_PRICE, DB_ITEM_COST, DB_ITEM_DESCRIPTION);
+    ItemCategory itemCategory = new ItemCategory(DB_ITEM_CATEGORY_TYPE, DB_ITEM_CATEGORY_NAME, DB_ITEM_CATEGORY_ICON);
+
+    @Before
+    public void setup(){
+        ItemDTO itemDto = new ItemDTO(DB_ITEM_NAME, DB_ITEM_PRICE, DB_ITEM_COST);
+    }
 
     @Test(expected = ItemCategoryNameDoesntExists.class)
     public void Create_item_with_non_existing_category_name() throws Exception {
 
-
+        when(itemCategoryRepository.findItemCategoryByCategoryName(DB_NEW_ITEM_CATEGORY_NAME)).thenReturn(itemCategory);;
+        ItemDTO newItemDto = itemService.create(itemDto);
     }
+
+    @Test(expected = ItemWithNameAlreadyExistsException.class)
+    public void Create_item_with_non_existing_name(){
+        Item newItem = new DrinkItem();
+        when(itemRepository.findByName(DB_NEW_ITEM_NAME)).thenReturn(newItem);
+    }
+
+    @Test
+    public void Create_food_item_success(){
+        Item newItem = new DrinkItem(DB_DRINK_ITEM_NAME, DB_DRINK_ITEM_PRICE, DB_DRINK_ITEM_COST);
+        when(itemService.create(itemDto));
+    }
+
+
+
 }
 
 //  @Test(expected = UserWithUsernameAlreadyExistsException.class)
